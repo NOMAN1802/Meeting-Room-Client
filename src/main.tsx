92f31cfcd703e64f7b717bfc937141af58f127ec
NOMAN1802 <mustakimnoman82@gmail.com>
2024-08-25 02:05:41 +0600

    initial project set up router & redux

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.ts'
+import { Toaster } from "sonner";
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/routes.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Provider store={store}>
+    <Toaster position="top-center" />
+    <RouterProvider router={router}/>
+    </Provider>,
+  </StrictMode>,
+)
